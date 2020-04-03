4555255b4555495773fbdc03b60776f2ad0de1b3
Naman Agarwal <namana019@outlook.com>
2020-04-03 17:43:32 +0530

    Editor working fine, need to integrate font api

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Writer from '../writer/writer';
+import Input from '../input/input';
 import './editor.scss';
 
 class Editor extends React.Component{
@@ -13,7 +14,9 @@ class Editor extends React.Component{
             value : [],
             top: [],
             left: [],
-            textCount: 0
+            textCount: 0,
+            color: '',
+            size: ''
         }
     }
 
@@ -21,6 +24,12 @@ class Editor extends React.Component{
         let value = this.state.value;
         value[type] = event.target.value;
         this.setState({value : value})
+    }
+
+    onOtherInputHandler = (event,type) => {
+        let dupState = this.state;
+        dupState[type] = event.target.value;
+        this.setState({dupState})
     }
 
     onInsertText = (event) => {
@@ -32,26 +41,22 @@ class Editor extends React.Component{
     }
 
     render(){
-        let {id, url, options, value, textCount, top, left} = this.state;
-        let option = [];
+        let {id, url, value, textCount, top, left, color, size} = this.state;
         let count = [];
-        for(let i=0; i<options; i++){
-            option[i] = i
-        }
         for(let j=0; j<textCount; j++){
-            count[j] = j
+            count[j] = j;
         }
 
         return(
             <div className="Editor">
                 <div className="Editor--ImgContainer">
                     <img src={url} alt={id} className="Editor--Img" />
-                    <div className="Editor--ImgInsertText" onClick={e => this.onInsertText(e)}>
+                    <div className="Editor--ImgInsertText" onDoubleClick={e => this.onInsertText(e)}>
                         <div className="Editor--ImgInsertTextContainer">
                             {
                                 count.map((opt,index) => {
                                     return(
-                                        <Writer key={index} top={top[index]} left={left[index]} changed={event => this.onChangeHandler(event,index)} value={value[index]} />
+                                        <Writer color={color} size={size} key={index} top={top[index]} left={left[index]} changed={event => this.onChangeHandler(event,index)} value={value[index]} />
                                         )
                                 })
                             }
@@ -59,7 +64,9 @@ class Editor extends React.Component{
                     </div>
                 </div>
                 <div className="Editor--Content">
-
+                    <h1 className="Editor--ContentHeadline">Triple Click to edit the meme!</h1>
+                    <Input type="text" value={color} placeholder="Choose the color" changed={event => this.onOtherInputHandler(event, "color")} />
+                    <Input type="text" value={size} placeholder="Font Size in pixels" changed={event => this.onOtherInputHandler(event, "size")} />
                 </div>
             </div>
         )
--- src/components/writer/writer.jsx
+++ src/components/writer/writer.jsx
@@ -4,7 +4,7 @@ import './writer.scss';
 const Writer = (props) => {
 
     return(
-            <input className="Writer" onClick={props.clicked} style={{position: 'absolute', top: props.top, left: props.left}} value={props.value} onChange={props.changed} type="text" placeholder="Enter text" />
+            <textarea rows="3" cols="15" className="Writer" onClick={props.clicked} style={{position: 'absolute', fontSize: `${props.size + "px"}`, color: props.color, top: props.top, left: props.left}} value={props.value} onChange={props.changed} type="text" />
     )
 }
 
