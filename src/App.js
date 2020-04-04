943cc474ce897d525a6f6dd744d5afa6a93fe417
Naman Agarwal <namana019@outlook.com>
2020-04-04 16:07:54 +0530

    Mememaker working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {Switch, Route, withRouter} from "react-router-dom";
-import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import Navbar from "./components/navbar/navbar";
-import SignIn from './pages/signup-login/signup-login';
 import Memepage from './pages/memepage/memepage';
 import './App.css';
 
@@ -15,39 +13,12 @@ class App extends React.Component {
     }
   }
 
-  unsubscribeFromAuth = null;
-
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
-        const userRef = await createUserProfileDocument(userAuth);
-
-        userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser : {
-              id : snapshot.id,
-              ...snapshot.data()
-            }
-            }, () => console.log(this.state))
-            this.props.history.push('/');
-        })
-      }
-      else{
-        this.setState({currentUser : null})
-      }
-    })
-  }
-
-  componentWillUnmount(){
-    this.unsubscribeFromAuth();
-  }
 
   render(){
     return (
       <div className="App">
-        <Navbar currentUser={this.state.currentUser}/>
+        <Navbar/>
         <Switch>
-          <Route exact path='/signin' component={SignIn} />
           <Route exact path="/" render={() => {
             return(
               <Memepage/>
