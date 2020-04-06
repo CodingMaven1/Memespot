e08349ba3a0b495ff253cbe3a3b05b0954fcbf06
Naman Agarwal <namana019@outlook.com>
2020-04-06 17:17:49 +0530

    Made the build

---
+++ build/precache-manifest.d3f3bde7790da6d845d9817b59787c99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b39b495c858fe6578c55b1edcd8bbf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70a924d0f3c50d1b14e3",
+    "url": "/static/css/main.ca646c64.chunk.css"
+  },
+  {
+    "revision": "9c88c00d1c09ccc9da44",
+    "url": "/static/js/2.c9e539f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c9e539f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a924d0f3c50d1b14e3",
+    "url": "/static/js/main.29c7bb41.chunk.js"
+  },
+  {
+    "revision": "0916ca7d65fd8573c84f",
+    "url": "/static/js/runtime-main.19368e87.js"
+  },
+  {
+    "revision": "cac43ed5757b676b8a56f54eebb175b0",
+    "url": "/static/media/Impact.cac43ed5.woff"
+  },
+  {
+    "revision": "e442d8033b3582950741fff89486b6dc",
+    "url": "/static/media/Impact.e442d803.woff2"
+  }
+]);
